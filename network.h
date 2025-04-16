c57b3d5733a263dea3964503ff3694a844303a92
Ruskaruma <154019945+ruskaruma@users.noreply.github.com>
2025-04-16 15:41:54 +0530

    removed redundancies

--- network.h
+++ network.h
@@ -3,11 +3,9 @@
 #define INPUT_LAYER 2
 #define HIDDEN_LAYER 3
 #define OUTPUT_LAYER 1
-
 void initialize_weights_and_biases();
 void feedforward(float input[]);
 void backpropagate(float input[], float target[], float learning_rate);
 void train(float inputs[][INPUT_LAYER], float targets[][OUTPUT_LAYER], int num_samples, int epochs, float learning_rate);
 void test(float input[]);
-
 #endif
