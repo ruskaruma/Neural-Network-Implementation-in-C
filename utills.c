3c32fe01142a77898bc7e614222730b178a83de3
Ruskaruma <154019945+ruskaruma@users.noreply.github.com>
2025-04-16 17:09:39 +0530

    update utils.c

--- utills.c
+++ utills.c
@@ -6,8 +6,8 @@ float sigmoid(float x)
     return 1.0f/(1.0f+expf(-x));
 }
 float dsigmoid(float y) {
-    return y * (1.0f - y);
+    return y*(1.0f-y);
 }
 float random_weight() {
-    return ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+    return ((float)rand()/RAND_MAX)*2.0f-1.0f;
 }
