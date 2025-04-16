845e58c313cf127e271e9358edc725d25cbb2d1f
Ruskaruma <154019945+ruskaruma@users.noreply.github.com>
2025-04-16 15:49:00 +0530

    update network.h

--- network.h
+++ network.h
@@ -1,11 +1,11 @@
-#ifndef NETWORK_H
-#define NETWORK_H
-#define INPUT_LAYER 2
-#define HIDDEN_LAYER 3
-#define OUTPUT_LAYER 1
+#ifndef network.h
+#define network.h
+#define inputLayer 2
+#define hiddenLayer 3
+#define outputLayer 1
 void initialize_weights_and_biases();
 void feedforward(float input[]);
-void backpropagate(float input[], float target[], float learning_rate);
-void train(float inputs[][INPUT_LAYER], float targets[][OUTPUT_LAYER], int num_samples, int epochs, float learning_rate);
+void backpropagate(float input[], float target[], float learningRate);
+void train(float inputs[][inputLayer], float targets[][outputLayer], int numSamples, int epochs, float learningRate);
 void test(float input[]);
 #endif
